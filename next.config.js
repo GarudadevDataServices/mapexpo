2ba0b6960076105fa476f7f03d3953fc25da809f
datta07 <akula.gurudatta@gmail.com>
2024-01-06 21:36:24 +0530

    gu/other reference

--- next.config.js
+++ next.config.js
@@ -1,29 +1,33 @@
 const SITE = require('./src/config.js').SITE;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  }
-  
-module.exports = nextConfig
-// module.exports = {
-//   reactStrictMode: true,
-
-//   trailingSlash: SITE.trailingSlash,
-//   basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
-
-//   swcMinify: true,
-//   poweredByHeader: false,
+// const nextConfig = {
 //   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.unsplash.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'source.unsplash.com',
-//       },
-//     ],
-//   }
+//     loader: 'akamai',
+//     path: '',
+//   },
+//   assetPrefix: './',
 // };
+
+// module.exports =  nextConfig;
+module.exports = {
+  reactStrictMode: true,
+
+  trailingSlash: SITE.trailingSlash,
+  basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
+
+  swcMinify: true,
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
+  }
+};
