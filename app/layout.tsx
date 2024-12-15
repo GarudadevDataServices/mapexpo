55ff20b7d956b798b241a575872f0e81ddc0f288
datta07 <akula.gurudatta@gmail.com>
2024-12-15 22:11:18 +0530

    added build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Script from 'next/script'
 
 import { SITE } from '~/config.js';
 
@@ -28,10 +29,13 @@ export default function RootLayout({ children }: LayoutProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="akuladatta" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
-      </head>
+       </head>
       <body className="bg-slate-100  tracking-tight text-gray-900 antialiased dark:bg-slate-900 dark:text-slate-300">
-        
+      <Script 
+        data-name="BMC-Widget" 
+        data-cfasync="false" 
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="akuladatta" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></Script>
+       
           <Header />
           <main className=''>{children}</main>
           <Footer2 />
