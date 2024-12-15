41a9331dcb2318da57afdacc856c4b8a49fc006e
datta07 <akula.gurudatta@gmail.com>
2024-12-15 21:55:34 +0530

    gu/added buy me a coffee

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }: LayoutProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="akuladatta" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </head>
       <body className="bg-slate-100  tracking-tight text-gray-900 antialiased dark:bg-slate-900 dark:text-slate-300">
         
